95a4fd60cce4552cddaae4b4827948029f98dca7
Maxime De Greve <maximedegreve@me.com>
2019-04-26 16:57:51 +0100

    Added carthages built with Swift 5

--- SwiftlineTests/Carthage/Build/Mac/Nimble.framework/Versions/A/Headers/Nimble.h
+++ SwiftlineTests/Carthage/Build/Mac/Nimble.framework/Versions/A/Headers/Nimble.h
@@ -3,5 +3,11 @@
 #import "NMBStringify.h"
 #import "DSL.h"
 
+#if TARGET_OS_TV
+    #import "CwlPreconditionTesting_POSIX.h"
+#else
+    #import "CwlPreconditionTesting.h"
+#endif
+
 FOUNDATION_EXPORT double NimbleVersionNumber;
 FOUNDATION_EXPORT const unsigned char NimbleVersionString[];
