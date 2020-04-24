d58d7cc9e2e75bd6d622bdacc94ea82e940a1eb0
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-04-24 13:41:04 +0200

    set platform (minimun requirement)

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
   name: "Swiftline",
+  platforms: [
+      .macOS(.v10_13),
+  ],
   products: [
     .library(name: "Swiftline", targets: ["Swiftline"]),
   ],
