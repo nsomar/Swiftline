bf2f035759dbfec3aad7b4fff2eb5d002a955b6d
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-04-27 17:30:04 +0200

    make swiftline compatible with swift-package

--- Package.swift
+++ Package.swift
@@ -1,15 +1,24 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
-  name: "Swiftline",
-  platforms: [
-      .macOS(.v10_13),
-  ],
-  products: [
-    .library(name: "Swiftline", targets: ["Swiftline"]),
-  ],
-  targets: [
-    .target(name: "Swiftline"),
-  ]
+    name: "Swiftline",
+    platforms: [
+        .macOS(.v10_13),
+    ],
+    products: [
+        .library(name: "Swiftline", targets: ["Swiftline"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick.git", from: "2.2.0"),
+        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.7"),
+    ],
+    targets: [
+        .target(name: "Swiftline"),
+        .testTarget(name: "SwiftlineTests",
+                    dependencies: [.target(name: "Swiftline"),
+                                   .product(name: "Quick"),
+                                   .product(name: "Nimble")],
+                    path: "SwiftlineTests"),
+    ]
 )
