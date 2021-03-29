2fb585b6fcf722223668ca0cba5a869dc49547ff
nadjem <mejdanmc@gmail.com>
2021-03-30 01:58:15 +0200

    fix package

--- Package.swift
+++ Package.swift
@@ -4,4 +4,23 @@ import PackageDescription
 
 let package = Package(
     name: "Swiftline"
+    products: [
+            .library(
+                name: "Swiftline",
+                targets: ["Swiftline"])
+        ],
+        targets: [
+            .target(
+                name: "Swiftline",
+                swiftSettings: [
+                    .define("DEBUG", .when(configuration: .debug)),
+                ]),
+            .testTarget(
+                name: "SwiftlineTests",
+                dependencies: ["Swiftline"]),
+            .testTarget(
+                name: "GeneralTests",
+                dependencies: ["Swiftline"]),
+        ],
+        swiftLanguageVersions: [.v4_2, .v5]
 )
